a9d2637f27cfb0932455a266e438a2f622263d14
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2021-03-20 17:03:40 +0100

    Started working on fullstack stuff, but had issues getting the file content

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
 const app = express();
 const dotenv = require("dotenv");
 const port = 3000;
 
 dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
+const upload = multer({
+  dest: "./uploads",
 })
 
-app.post('/upload')
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.get('/users', function (req, res) {
+  fs.readFile('users.json', 'utf8', ((err, data) => {
+    res.end(data)
+  }))
+})
+
+app.post("/upload",(req, res) => {
+  fs.appendFile("file.txt", res.send)
+
+});
+
+app.get("/upload", ((req, res) => {
+  res.send("Hello from outer space.")
+}))
 
 app.listen(port, () => {
   console.log(`Server now listens to port ${port}`);
--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,30 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    fileContent: "Hello there, this is text in a fake file, coming from store."
+    fileContent: ""
   },
   mutations: {
     setFileContent(state, value) {
       state.fileContent = value;
     }
   },
-  actions: {},
+  actions: {
+    async uploadFile({ commit }, file) {
+      let response = await fetch("/upload", {
+        method: "post",
+        body: file
+      });
+      console.log(response)
+      let newFile = await response.json();
+      console.log(newFile);
+      commit("setFileContent", newFile);
+    },
+    async getTextFromFile({commit}) {
+      let response = await fetch("/upload");
+      let data = await response.text()
+      console.log(data);
+      commit("setFileContent", data)
+    }
+  },
   modules: {}
 });
